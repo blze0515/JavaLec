57584d99c693dc238cc11aca1003c7393bf634fe
blze0515 <blze0515@naver.com>
2023-02-10 13:29:31 +0900

    230210 4교시

--- JavaProgramming/src/chap03_operator/_02_twoOpOperator.java
+++ JavaProgramming/src/chap03_operator/_02_twoOpOperator.java
@@ -14,19 +14,19 @@ public class _02_twoOpOperator {
 		int num2 = 7;
 		int result;
 		
-		result = num1 + num2;
+		result = num1 + num2; //37
 		System.out.println("num1 + num2 = " + result);
 		
-		result = num1 - num2;
+		result = num1 - num2; //23
 		System.out.println("num1 - num2 = " + result);
 		
-		result = num1 * num2;
+		result = num1 * num2; //210
 		System.out.println("num1 * num2 = " + result);
 		
-		result = num1 / num2;
+		result = num1 / num2; //4
 		System.out.println("num1 / num2 = " + result);
 		
-		result = num1 % num2;
+		result = num1 % num2; //2
 		System.out.println("num1 % num2 = " + result);
 		System.out.println("-----------------------------------");
 		
@@ -34,20 +34,21 @@ public class _02_twoOpOperator {
 		int num3 = 10;
 		int num4 = 3;
 		
-		num3 += num4;
-		System.out.println("num3 += num4" + num3);
+		num3 += num4; //num3 = num3 + num4; => 13
+		System.out.println("num3 += num4 = " + num3);
 		
-		num3 -= num4;
-		System.out.println("num3 -= num4" + num3);
+		num3 -= num4; //num3 = num3 - num4; => 13 - 3 = 10 
+		System.out.println("num3 -= num4 = " + num3);
 		
-		num3 *= num4;
-		System.out.println("num3 *= num4" + num3);
+		num3 *= num4; //num3 = num3 * num4; => 10 * 3 = 30
+		System.out.println("num3 *= num4 = " + num3);
 
-		num3 /= num4;
-		System.out.println("num3 /= num4" + num3);
+		num3 /= num4; //num3 = num3 / num4; => 30 / 3 = 10
+		System.out.println("num3 /= num4 = " + num3);
 		
-		num3 %= num4;
-		System.out.println("num3 %= num4" + num3);
+		num3 %= num4; //num3 = num3 % num4; => 10 % 3 = 1
+		System.out.println("num3 %= num4 = " + num3);
+		System.out.println("-----------------------------------");
 		
 		//4. 관계연산자
 		int num5 = 100;
@@ -55,24 +56,40 @@ public class _02_twoOpOperator {
 		boolean result2;
 		
 		result2 = num5 < num6;
-		System.out.println("num5 < num6 = " + result2);
+		System.out.println("num5 < num6 = " + result2); //true
 		
 		result2 = num5 > num6;
-		System.out.println("num5 > num6 = " + result2);
+		System.out.println("num5 > num6 = " + result2); //false
 		
 		result2 = num5 <= num6;
-		System.out.println("num5 <= num6 = " + result2);
+		System.out.println("num5 <= num6 = " + result2); //true
 		
 		result2 = num5 >= num6;
-		System.out.println("num5 >= num6 = " + result2);
+		System.out.println("num5 >= num6 = " + result2); //false
 		
 		result2 = num5 == num6;
-		System.out.println("num5 == num6 = " + result2);
+		System.out.println("num5 == num6 = " + result2); //false
 		
 		result2 = num5 != num6;
-		System.out.println("num5 != num6 = " + result2);
+		System.out.println("num5 != num6 = " + result2); //true
+		System.out.println("-----------------------------------");
 		
 		//4. 논리연산자
+		boolean result3;
+		
+		result3 = 43 >= 81 && 62 < 99;
+		System.out.println("43 >= 81 && 62 < 99 = " + result3); //false
+		
+		result3 = false || num5 == (num6 / 3);
+		System.out.println("false || num3 == (num4 / 3) = " + result3); //true
+		
+		System.out.println("!result3 = " + !result3); //false
+		
+		
+		
+		
+		
+		
 	}
 
 }
